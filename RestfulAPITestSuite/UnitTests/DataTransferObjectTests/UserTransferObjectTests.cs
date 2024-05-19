add1e6833c69fa27a11e19ee052b7b0b06d1fc87
Bradley Watkins <watkinsbradley01@gmail.com>
2024-05-19 14:00:59 +0100

    Refactor name of DTO tests

--- RestfulAPITestSuite/UnitTests/DataTransferObjectTests/UserTransferObjectTests.cs
+++ RestfulAPITestSuite/UnitTests/DataTransferObjectTests/UserTransferObjectTests.cs
@@ -9,23 +9,23 @@ using Workout_API.Models;
 
 namespace Workout_API_Test_Suite.UnitTests.DataTransferObjectTests
 {
-    public class UserTransferObjectTests
+    public class DataTransferObjectTests
     {
-        const string Name = "UserTransferObject";
-        const string Email = "UserTransferObject@gmail.com";
-        const int Id = 10;
-
         [Fact]
         public void ConvertUserTransferObjectToUserObject()
         {
+            const string Name = "UserTransferObject";
+            const string Email = "UserTransferObject@gmail.com";
+            const int Id = 10;
+
             UserTransferObject userTransferObject = new UserTransferObject();
             userTransferObject.Id = Id;
             userTransferObject.Name = Name;
             userTransferObject.Email = Email;
 
             User userObject = (User)userTransferObject.ToObject();
-
             Assert.NotNull(userObject);
+
             userObject.Id.Should().Be(Id);
             userObject.Name.Should().Be(Name);
             userObject.Email.Should().Be(Email);
