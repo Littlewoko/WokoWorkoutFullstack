6a2d12cd86a8516a79843bf72c50bf9483a928ec
Bradley Watkins <watkinsbradley01@gmail.com>
2024-06-09 19:56:34 +0100

    NOT WORKING: look into x uinit dependency injection (or how to use EF inside xUnit)

---
+++ RestfulAPI/Models/UserStore.cs
@@ -0,0 +1,14 @@
+﻿using Workout_API.DBContexts;
+
+namespace Workout_API.Models
+{
+    public class UserStore
+    {
+        private readonly DBContext _context;
+
+        public UserStore(DBContext dBContext)
+        {
+            _context = dBContext;
+        }
+    }
+}
---
+++ RestfulAPITestSuite/UnitTests/ModelTests/StorageTests.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Workout_API.DBContexts;
+using Workout_API.Models;
+
+namespace Workout_API_Test_Suite.UnitTests.ModelTests
+{
+    public class StorageTests
+    {
+        DBContext DBContext { get; set; }
+
+        public StorageTests(DBContext _context)
+        {
+            DBContext = _context;
+        }
+
+        [Fact]
+        public void CreateStorageObject()
+        {
+            UserStore store = new UserStore(DBContext);
+            Assert.NotNull(store);
+        }
+    }
+}
